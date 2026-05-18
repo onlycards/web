4b7e9cf91afa7100f58556ccb71dc2af0b75b086
Artem <nelkor@vk.com>
2026-05-18 16:54:27 +0300

    links (#6)

--- packages/landing/src/main.tsx
+++ packages/landing/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import './index.scss'
+
 const root = document.getElementById('root')
 
 if (!root) {
@@ -9,6 +11,18 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <></>
+    <ul>
+      <li>
+        <a href="https://docs.onlycards.online">Docs</a>
+      </li>
+
+      <li>
+        <a href="https://admin.onlycards.online">Admin</a>
+      </li>
+
+      <li>
+        <a href="https://app.onlycards.online">App</a>
+      </li>
+    </ul>
   </StrictMode>,
 )
