077351be69e4558ad88e12eb7abd883718ebd6ab
a.tutunnik <a.tutunnik@corp.mail.ru>
2026-05-22 17:05:45 +0300

    subj

--- packages/ui/src/card/index.ts
+++ packages/ui/src/card/index.ts
@@ -1,4 +1,5 @@
 import './index.scss'
 
+export type { FlipCardProps } from './types'
 export { CardFace } from './components/CardFace'
 export { FlipCard } from './components/FlipCard'
--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -1,4 +1,5 @@
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 
+export type { FlipCardProps } from '@/card'
 export { CardFace, FlipCard } from '@/card'
